bfdd8e534122056e51abc28e19836a95d2da27a9
jae9380 <ljy5314@gmail.com>
2024-10-27 20:39:24 +0900

    Test: HealthCheckController HealthCheckController 테스트 코드 작성

---
+++ src/test/java/com/example/demo/controller/HealthCheckControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureTestDatabase
+class HealthCheckControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void healthCheck_응답_결과_200() throws Exception {
+         mockMvc.perform(get("/health_check.html"))
+                 .andExpect(status().isOk());
+    }
+}
